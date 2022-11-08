4319613d6a0ff9c7a69a90b30fb4b1dcbbc94f61
Anna Sudol <anjasudol@gmail.com>
2022-11-08 11:55:42 +0000

    app init

---
+++ pages/api/verify.ts
@@ -0,0 +1,25 @@
+
+
+import { v4 as uuidv4 } from "uuid";
+import { Session } from "next-iron-session";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withSession, contractAddress } from "./utils";
+
+export default withSession(async (req: NextApiRequest & { session: Session }, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      console.log(req.session, 'req.method')
+
+      const message = { contractAddress, id: uuidv4() };
+      console.log(message, 'message')
+      req.session.set("message-session", message);
+      await req.session.save();
+
+      res.json(message);
+    } catch {
+      res.status(422).send({ message: "Cannot generate a message!" });
+    }
+  } else {
+    res.status(200).json({ message: "Invalid api route" });
+  }
+})
